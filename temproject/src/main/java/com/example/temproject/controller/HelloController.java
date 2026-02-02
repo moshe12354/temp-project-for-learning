95af2b74a643fbc5dd221a3a90644b7fc9789422
takfi <takfir719@gmail.com>
2026-02-02 12:37:34 +0200

    commit content

--- temproject/src/main/java/com/example/temproject/controller/HelloController.java
+++ temproject/src/main/java/com/example/temproject/controller/HelloController.java
@@ -1,19 +1,25 @@
 package com.example.temproject.controller;
-
-import org.springframework.http.HttpStatus;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
+@Api(tags = "Hello API")
 @RestController
-    @RequestMapping("/api/hello")
-    public class HelloController {
+@RequestMapping("/api/hello")
+public class HelloController {
 
-        @RequestMapping(value = "", method = RequestMethod.GET)
-        public ResponseEntity<?> hello(){
-            return new ResponseEntity<>("Hello World!", HttpStatus.OK);
-        }
-
+    @ApiOperation(value = "Say Hello")
+    @GetMapping("")
+    public ResponseEntity<Map<String, String>> hello() {
+        return ResponseEntity.ok(Collections.singletonMap("message", "Hello World!"));
     }
+}
+
+
 
