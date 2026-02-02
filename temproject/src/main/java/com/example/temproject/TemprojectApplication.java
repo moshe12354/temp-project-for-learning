f484525b9b4dcd130f33a8725ea6da70ae65b8c3
takfi <takfir719@gmail.com>
2026-02-02 10:53:38 +0200

    commit content

---
+++ temproject/src/main/java/com/example/temproject/TemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.temproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemprojectApplication.class, args);
+	}
+
+}
